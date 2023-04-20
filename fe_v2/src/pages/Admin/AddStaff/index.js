c9fd33de337c502b1e37f08df97ed304ba312f26
LeHaiDangSPKT <0509dangle@gmail.com>
2023-04-20 10:41:04 +0700

    Management staff

--- fe_v2/src/pages/Admin/AddStaff/index.js
+++ fe_v2/src/pages/Admin/AddStaff/index.js
@@ -1,15 +1,56 @@
 import React from "react";
 import "./index.css";
 import Button from "../../../component/Button";
+import Axios from "axios";
 
 const AddStaff = () => {
+  const staff = JSON.parse(localStorage.getItem("staff"));
+  const [inforStaff, setInforStaff] = React.useState({
+    name: staff.name || "",
+    email: staff.email || "",
+    password: staff.password || "",
+  });
+  const url = window.location.pathname;
+  const isAdd = url.includes("add-staff");
+
+  const addStaff = (e) => {
+    if (!inforStaff.email.includes("@student.hcmute.edu.vn")) {
+      alert("Email phải thuộc hệ thống HCMUTE");
+      return;
+    }
+    Axios.post(`${process.env.REACT_APP_API}/admin/addStaff`, inforStaff)
+      .then((res) => {
+        alert("Thêm nhân viên thành công");
+        window.location.href = "/admin";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const editStaff = (e) => {
+    if (!inforStaff.email.includes("@student.hcmute.edu.vn")) {
+      alert("Email phải thuộc hệ thống HCMUTE");
+      return;
+    }
+    Axios.put(
+      `${process.env.REACT_APP_API}/admin/editStaff/${staff._id}`,
+      inforStaff
+    )
+      .then((res) => {
+        alert("Chỉnh sửa nhân viên thành công");
+        window.location.href = "/admin";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const button = [
     {
-      name: "Thêm nhân viên",
+      name: isAdd ? "Thêm nhân viên" : "Cập nhật",
       type: 1,
-      onClick: () => {
-        alert("Thêm nhân viên thành công");
-      },
+      onClick: isAdd ? () => addStaff() : () => editStaff(),
     },
     {
       name: "Hủy",
@@ -22,31 +63,44 @@ const AddStaff = () => {
 
   return (
     <div class="input__container">
-      <div>
+      <div className="input__label__container">
         <label class="input__label">Họ Tên</label>
         <input
           placeholder="Nhập họ và tên"
           class="input"
-          name="text"
+          name="name"
           type="text"
+          value={inforStaff.name}
+          onChange={(e) =>
+            setInforStaff({ ...inforStaff, name: e.target.value })
+          }
         ></input>
       </div>
-      <div>
+      <div className="input__label__container">
         <label class="input__label">Email</label>
         <input
           placeholder="Nhập email"
           class="input"
-          name="text"
+          name="email"
           type="email"
+          value={inforStaff.email}
+          onChange={(e) =>
+            setInforStaff({ ...inforStaff, email: e.target.value })
+          }
         ></input>
       </div>
-      <div>
+      <div className="input__label__container">
         <label class="input__label">Mật khẩu</label>
         <input
           placeholder="Nhập mật khẩu"
           class="input"
-          name="text"
-          type="text"
+          name="password"
+          readOnly={!isAdd}
+          type="password"
+          value={inforStaff.password}
+          onChange={(e) =>
+            setInforStaff({ ...inforStaff, password: e.target.value })
+          }
         ></input>
       </div>
 
